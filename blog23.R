d7ad2c2c1450db405ae906016584669f73999233
blogakademin <barablog23@outlook.com>
2023-08-25 15:10:42 +0200

    initial build

---
+++ blog23.R
@@ -0,0 +1,8 @@
+library(distill)
+library(blogdown)
+library(postcards)
+
+create_website(dir=".",title = "blog23",gh_pages = T)
+
+create_article(file="index",template = "trestles",
+               package = "postcards")
