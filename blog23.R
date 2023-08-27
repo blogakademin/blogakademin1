5c7eb702a39ef0d040411da06516ee108025aa61
Xin Xia <barablog23>
2023-08-27 19:33:29 +0200

    updates

--- blog23.R
+++ blog23.R
@@ -7,4 +7,9 @@ create_website(dir=".",title = "blog23",gh_pages = T)
 create_article(file="index",template = "trestles",
                package = "postcards")
 
+create_article(file="guid1")
 
+
+create_post("Diabetes guidelines for older adults")
+
+create_post("Cardiovascular disease prevention guidelines for older adults")
